123b5ded867e0b17d9245d70839120943801f192
Saad Mahmud <shuvro.mahmud79@gmail.com>
2022-11-05 10:07:18 +0600

    Add untested first draft

--- lex/lex_stack.py
+++ lex/lex_stack.py
@@ -6,21 +6,20 @@ from aws_cdk import (
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
+    aws_lex as lex,
 )
 
 
 class LexStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "LexQueue",
-            visibility_timeout=Duration.seconds(300),
+        # Lex bot draft 1
+        cfn_bot = lex.CfnBot(
+            self,
+            "MyLexBot",
+            data_privacy=False,
+            idle_session_ttl_in_seconds=123,
+            name="FirstLexBot",
+            role_arn="role_arn",
         )
-
-        topic = sns.Topic(
-            self, "LexTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
