d9297bd6ba3eac4d68a5a574b4eba6f2e9454c72
Saad Mahmud <shuvro.mahmud79@gmail.com>
2022-11-06 17:59:11 +0600

    Set Slot Priorities

--- lex/lex_stack.py
+++ lex/lex_stack.py
@@ -98,7 +98,11 @@ class LexStack(Stack):
             description="Intent to order a bouquet of flowers for pickup",
             sample_utterances=[utterance_flowers_a, utterance_flowers_b],
             intent_confirmation_setting=confirm_flowers,
-            slot_priorities=[],
+            slot_priorities=[
+                pickup_date_priority,
+                flower_type_priority,
+                pickup_time_priority,
+            ],
         )
 
         # localeProperty
